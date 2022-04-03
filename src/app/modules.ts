22c7c89b15d74ed9c4fd82707f04d62a2b7f1179
dgkhoa89 <dgkhoa89@gmail.com>
2022-04-03 14:03:13 +0700

    add models, http service, environment variable

---
+++ src/app/modules.ts
@@ -0,0 +1,7 @@
+export interface Game{
+
+}
+
+export interface APIResponse<T>{
+    results: Array<T>
+}
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { APIResponse, Game } from '../modules';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getGameList(ordering :string, search?:string):Observable<APIResponse<Game>> {
+    let params = new HttpParams().set('ordering',ordering)
+    if(search){
+      params.append('search',search)
+    }
+    return this.http.get<APIResponse<Game>>(environment.baseUrl + '/games',{params:params});
+  }
+
+}
