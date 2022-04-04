624b0d39541157f408767a68d5716e4db8080bd6
dgkhoa89 <dgkhoa89@gmail.com>
2022-04-04 23:33:02 +0700

    create detail component, update app-routing define structure of details.component.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './components/details/details.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
@@ -7,7 +8,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'search/:game-search', component: HomeComponent },
-    { path: 'details/:gameId', component: HomeComponent },
+    { path: 'details/:gameId', component: DetailsComponent },
     { path: '**', component: PageNotFoundComponent }
 ];
 
